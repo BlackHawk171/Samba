2dd9e2e3ae2e2ca2db65f678401716fa4247ce13
BlackHawk171 <kpuusild@gmail.com>
2017-10-20 08:34:08 +0300

    Update samba.bash

--- samba.bash
+++ samba.bash
@@ -13,44 +13,25 @@ fi
 
 clear
 
-echo "Palun sisesta virtuaal masina nimi"
-echo "Näiteks: fs"
-read $masinanimi
 
-clear
-
-
-echo "Palun sisesta WORKGROUP nimi"
-echo "Näiteks: KEVIN"
-read $WORKGROUP
-
-clear
-
-echo "Palun sisesta REALM"
-echo "Näiteks: KEVIN.LAN"
-read $REALM
-
-clear
-
-
-echo "[global]" ˃ /etc/samba/smb.conf
-echo "" ˃˃ /etc/samba/smb.conf
-echo "       netbios name = "$masinanimi"" ˃˃ /etc/samba/smb.conf
-echo "       security = ADS" ˃˃ /etc/samba/smb.conf
-echo "       workgroup = "$WORKGROUP"" ˃˃ /etc/samba/smb.conf
-echo "       realm = "$REALM"" ˃˃ /etc/samba/smb.conf
-echo "" ˃˃ /etc/samba/smb.conf
-echo "       log file = /var/log/samba/%m.log" ˃˃ /etc/samba/smb.conf
-echo "       log level = 1" ˃˃ /etc/samba/smb.conf
-echo "" ˃˃ /etc/samba/smb.conf
-echo "       dedicated keytab file = /etc/krb5.keytab" ˃˃ /etc/samba/smb.conf
-echo "       kerberos method = secrets and keytab" ˃˃ /etc/samba/smb.conf
-echo "       winbind refresh tickets = yes" ˃˃ /etc/samba/smb.conf
-echo "" ˃˃ /etc/samba/smb.conf
-echo "       winbind use default domain = yes" ˃˃ /etc/samba/smb.conf
-echo "       winbind enum users  = yes" ˃˃ /etc/samba/smb.conf
-echo "       winbind enum groups = yes" ˃˃ /etc/samba/smb.conf
-echo "" ˃˃ /etc/samba/smb.conf
-echo " 	vfs objects = acl_xattr" ˃˃ /etc/samba/smb.conf
-echo "       map acl inherit = yes" ˃˃ /etc/samba/smb.conf
-echo "       store dos attributes = yes" ˃˃ /etc/samba/smb.conf
+echo "[global]" > /etc/samba/smb.conf
+echo "" >> /etc/samba/smb.conf
+echo "       netbios name = " ˃˃ /etc/samba/smb.conf
+echo "       security = ADS" >> /etc/samba/smb.conf
+echo "       workgroup = " >> /etc/samba/smb.conf
+echo "       realm = " >> /etc/samba/smb.conf
+echo "" >> /etc/samba/smb.conf
+echo "       log file = /var/log/samba/%m.log" >> /etc/samba/smb.conf
+echo "       log level = 1" >> /etc/samba/smb.conf
+echo "" >> /etc/samba/smb.conf
+echo "       dedicated keytab file = /etc/krb5.keytab" >> /etc/samba/smb.conf
+echo "       kerberos method = secrets and keytab" >> /etc/samba/smb.conf
+echo "       winbind refresh tickets = yes" >> /etc/samba/smb.conf
+echo "" >> /etc/samba/smb.conf
+echo "       winbind use default domain = yes" >> /etc/samba/smb.conf
+echo "       winbind enum users  = yes" >> /etc/samba/smb.conf
+echo "       winbind enum groups = yes" >> /etc/samba/smb.conf
+echo "" >> /etc/samba/smb.conf
+echo " 	vfs objects = acl_xattr" >> /etc/samba/smb.conf
+echo "       map acl inherit = yes" >> /etc/samba/smb.conf
+echo "       store dos attributes = yes" >> /etc/samba/smb.conf
